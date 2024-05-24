087d255da059677ad34a35a481f6726a39d11da5
Joaovvb <rm98938@fiap.com.br>
2024-05-23 22:59:23 -0300

    lifecyclemethods

--- src/App.js
+++ src/App.js
@@ -3,61 +3,59 @@ import './App.css';
 import {Component} from 'react'
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this);
-    this.state = {
-      name: 'João Vitor'
-    }
-  }
+  state = {
+      counter: 0,
+      post: [
+        {
+          id:1,
+          title: 'Title',
+          body: 'Body'
+        },
+        {
+          id:2,
+          title: 'Title 2',
+          body: 'Body 2'
+        },
+        {
+          id:3,
+          title: 'Title 3',
+          body: 'Body 3'
+        }
+      ]
+    };
 
-  handlePClick(){
-    this.setState({name : "Benjamin"})
-  }
+
+
+    componentDidMount(){
+     this.handleTimeout();
+    }
+
+    componentDidUpdate(){
+     // this.handleTimeout();
+    }
+
+    handleTimeout=() =>{
+      const {post, counter} = this.state;
+      post[0].title = "Titulo mudou"
+        setTimeout(() => {
+          this.setState({post, counter : counter + 1})
+        }, 1000);
+    }
 
   render(){
-    const {name} = this.state;
+    const {post, counter} = this.state;
      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick}>
-              {name}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+        {post.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
     </div>
   );
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
