2c4155e4f6a66bd39762d5302997a05a20690450
Joaovvb <rm98938@fiap.com.br>
2024-05-24 15:46:04 -0300

    Fetch Api

--- src/App.js
+++ src/App.js
@@ -3,53 +3,29 @@ import {Component} from 'react'
 
 class App extends Component{
   state = {
-      counter: 0,
-      post: [
-        {
-          id:1,
-          title: 'Title',
-          body: 'Body'
-        },
-        {
-          id:2,
-          title: 'Title 2',
-          body: 'Body 2'
-        },
-        {
-          id:3,
-          title: 'Title 3',
-          body: 'Body 3'
-        }
-      ]
+      post:[]
     };
 
 
 
     componentDidMount(){
-     this.handleTimeout();
+        this.loadPost();
     }
 
-    componentDidUpdate(){
-      this.handleTimeout();
-    }
+    loadPost = async () =>{
+      const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
 
-    componentWillUnmount(){
-      clearTimeout(this.handleTimeout);
-    }
+      const [post] = await Promise.all([postsResponse]);
 
-    handleTimeout=() =>{
-      const {post, counter} = this.state;
-      post[0].title = "Titulo mudou"
-        setTimeout(() => {
-          this.setState({post, counter : counter + 1})
-        }, 1000);
+      const postJson = await post.json();
+
+      this.setState({post: postJson})
     }
 
   render(){
-    const {post, counter} = this.state;
+    const {post} = this.state;
   return (
     <div className="App">
-      <h1>{counter}</h1>
         {post.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
